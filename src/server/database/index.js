c63d940b6fcb7b72b24d4311c93880068dc365ca
bkhebert <bondman2004tippy@gmail.com>
2025-04-02 13:34:07 -0500

    setup server

---
+++ src/server/database/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const distPath = path.resolve(__dirname, '..', '..', 'dist');
+app.use(express.static(distPath));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
+
+// SERVER CONNECTION
+app.listen(port, () => {
+  console.log(`\
+  Listening at:\n \n http://127.0.0.1:${port} \n\n http://localhost:${port}
+  `);
+});
