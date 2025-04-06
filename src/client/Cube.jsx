abfd5c59fa5aed3973bcac9aad857424a09828d7
bkhebert <bondman2004tippy@gmail.com>
2025-04-05 21:55:59 -0500

    ADD animated cube

--- src/client/Cube.jsx
+++ src/client/Cube.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 
 const Cube = ({position, size, color}) => {
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta * 2.0;
+    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
+    
+  })
 
   return (
-    <mesh position={position}>
+    <mesh position={position} ref={ref}>
           <boxGeometry args={size}></boxGeometry>
           <meshStandardMaterial color={color}></meshStandardMaterial>
     </mesh>
