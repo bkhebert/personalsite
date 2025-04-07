3a1808a9d334ab21411ec925e606c1406898e5ad
bkhebert <bondman2004tippy@gmail.com>
2025-04-07 01:24:40 -0500

    ADD Materials Component

--- src/client/Materials.jsx
+++ src/client/Materials.jsx
@@ -18,7 +18,16 @@ const Materials = () => {
     <fog attach="fog" args={[fogColor, 2, 10]}/>
         <mesh position={position} ref={ref}>
               <sphereGeometry args={size}></sphereGeometry>
-              <meshBasicMaterial color={color} opacity={opacity} transparent></meshBasicMaterial>
+              <meshBasicMaterial 
+              color={color} 
+              opacity={opacity} 
+              transparent
+             // depthTest={false} <-- When false, it will always be in front
+             // visible={false} <-- When false, makes item invisible 
+             // depthWrite={false} <-- When false, does not account whatever is closest to camera
+              // alphaTest={0.35} <-- Decides when the minimum opacity should render
+              />
+      
         </mesh>
     
     </>
